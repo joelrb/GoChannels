68f3c1f2a71901eb849848a5ecb349613977a18d
Joel Ballesteros <joelrb@gmail.com>
2018-08-01 17:55:47 +0800

    Make both Routines as sender and receiver

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ var wg = sync.WaitGroup{}
 
 func main() {
 	ch := make(chan int)
-	for j := 0; j < 5; j++ {
-		wg.Add(2)
-		go func() {
-			i := <-ch
-			fmt.Println(i)
-			wg.Done()
-		}()
-		go func() {
-			ch <- 42
-			wg.Done()
-		}()
-	}
+	wg.Add(2)
+	go func() {
+		i := <-ch
+		fmt.Println(i)
+		ch <- 27
+		wg.Done()
+	}()
+	go func() {
+		ch <- 42
+		fmt.Println(<-ch)
+		wg.Done()
+	}()
 	wg.Wait()
 
 }
