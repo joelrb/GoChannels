d41df8562931693b1c28fa18055bf488eeaf5f36
Joel Ballesteros <joelrb@gmail.com>
2018-08-02 16:58:24 +0800

    Closing channel.

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ func main() {
 	ch := make(chan int, 50) // With 50 buffer
 	wg.Add(2)
 	go func(ch <-chan int) { // Receive only channel
-		i := <-ch
-		fmt.Println(i)
-		i = <-ch
-		fmt.Println(i)
+		for i := range ch {
+			fmt.Println(i)
+		}
 		wg.Done()
 	}(ch)
 	go func(ch chan<- int) { // Send only channel
 		ch <- 42
 		ch <- 47
+		close(ch)
 		wg.Done()
 	}(ch)
 	wg.Wait()
